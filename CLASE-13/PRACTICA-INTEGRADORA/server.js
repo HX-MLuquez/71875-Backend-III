1e6c016ffd6d02ad8c3a34918191d9d072b0f3d6
Mauricio Gastón <mauuuricio@hotmail.com>
2025-06-17 19:48:47 -0300

    clase 13 practica integradora

---
+++ CLASE-13/PRACTICA-INTEGRADORA/server.js
@@ -0,0 +1,23 @@
+import app from "./src/app.js";
+import { connectDB } from "./src/config/db.js";
+
+import config from "./src/config/config.js";
+
+const { PORT } = config;
+
+
+const startServer = async () => {
+  await connectDB(); 
+
+  app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+    console.log(
+      `Connected to MongoDB at mongodb://localhost:27017/tdd_testing`
+    );
+    console.log(
+      `API documentation available at http://localhost:${PORT}/api-docs`
+    );
+  });
+};
+
+startServer();
